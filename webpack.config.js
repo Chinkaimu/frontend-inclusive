fea59c430e7dfcc30d91cae98b90d243f7854357
chinkaimu <823388255@qq.com>
2020-01-02 16:29:35 +0800

    feat: add promise practice

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,36 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        home: "./index",
-        practiceContext: "./projects/react/practice-context/index.js",
-        practiceContext1: "./projects/react/practice-context/index1.js",
-        practiceReact: "./projects/react/practice-react/index.js",
-        practiceRedux: "./projects/react/practice-redux/index.js",
-        practiceRedux1: "./projects/react/practice-redux1/index.js",
-        practiceRedux2: "./projects/react/practice-redux2/index.js",
-        practiceRedux3: "./projects/react/practice-redux3/index.js",
-    },
-    output: {
-        filename: "[name].bundle.js",
-        publicPath: "/dist/",
-        libraryTarget: "umd"
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
-            }
-        }]
-    },
-    watch: true,
-    devServer: {
-        compress: true,
-        port: 9000
-    },
-    devtool: 'cheap-module-eval-source-map',
+  mode: 'development',
+  entry: {
+    home: './index',
+    practiceContext: './code-projects/react/practice-context/index.js',
+    practiceContext1: './code-projects/react/practice-context/index1.js',
+    practiceReact: './code-projects/react/practice-react/index.js',
+    practiceRedux: './code-projects/react/practice-redux/index.js',
+    practiceRedux1: './code-projects/react/practice-redux1/index.js',
+    practiceRedux2: './code-projects/react/practice-redux2/index.js',
+    practiceRedux3: './code-projects/react/practice-redux3/index.js',
+    promise1: './code-javascript/promise1.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    publicPath: '/dist/',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    }]
+  },
+  watch: true,
+  devServer: {
+    compress: true,
+    port: 9000
+  },
+  devtool: 'cheap-module-eval-source-map'
 }
