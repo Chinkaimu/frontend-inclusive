cd4a512a7cffb6c199fa17060c51067e1badd2af
chinkaimu <823388255@qq.com>
2020-01-30 22:08:00 +0800

    feat: New simulation

---
+++ code-javascript/interview/new.js
@@ -0,0 +1,39 @@
+function Person (name, age, job) {
+  this.name = name
+  this.age = age
+  this.job = job
+  this.sayName = function () {
+    console.log(this.name)
+  }
+}
+console.log(new Person())
+
+// use object literal
+function createObject1 (constructor, ...res) {
+  const newObject = {}
+  newObject.__proto__ = constructor.prototype
+
+  const value = constructor.call(newObject, ...res)
+  return value || newObject
+}
+console.log(createObject1(Person))
+
+// use Object.create()
+function createObject (constructor, ...res) {
+  const newObject = Object.create(null)
+  newObject.__proto__ = constructor.prototype
+
+  const value = constructor.call(newObject, ...res)
+  return value || newObject
+}
+
+// use Object.setPrototypeOf()
+// https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/setPrototypeOf
+console.log(createObject(Person))
+const createNew = (Con, ...args) => {
+  const obj = {}
+  Object.setPrototypeOf(obj, Con.prototype)
+  const result = Con.apply(obj, args)
+  return result instanceof Object ? result : obj
+}
+console.log(createNew(Person))
