c5199ad6c74b11ff409a5e39923facef68656f37
chinkaimu <823388255@qq.com>
2020-02-01 18:52:31 +0800

    feat: debounce

--- code-javascript/interview/copys.js
+++ code-javascript/interview/copys.js
@@ -1,5 +1,5 @@
 /**
- * A deep copy means that all of the values of the new variable are copied and disconnected from the original variable. 
+ * A deep copy means that all of the values of the new variable are copied and disconnected from the original variable.
  * A shallow copy means that certain (sub-)values are still connected to the original variable.
  */
 /**
