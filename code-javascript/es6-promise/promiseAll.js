a4c295453572c9e4f9941a4114f5c28748838901
chinkaimu <823388255@qq.com>
2020-02-22 22:17:02 +0800

    feat: implete Promise.all

--- code-javascript/es6-promise/promiseAll.js
+++ code-javascript/es6-promise/promiseAll.js
@@ -31,3 +31,43 @@ function promiseAll (promises) {
 promiseAll([1, 2, 3]).then((value) => {
   console.log(value)
 })
+
+const myPromiseAll = (arr) => {
+  return new Promise((resolve, reject) => {
+    if (!Array.isArray(arr)) {
+      return reject(new TypeError('arguments must be an array'))
+    }
+
+    let resolvedCount = 0
+    const resolvedData = new Array(arr.length)
+
+    for (let i = 0; i < arr.length; i++) {
+      // 1. 错误点：需要用立即执行函数，确保 promise 的顺序没有变
+      (function (i) {
+        // 2. 错误点：增加 Promise.resolve 转化，如果是 Promise 对象则返回原对象，否则先转化成 Promise 对象
+        Promise.resolve(arr[i]).then((data) => {
+          resolvedData[i] = data
+          resolvedCount++
+          if (resolvedCount === arr.length) {
+            // 3. 错误点：没有 return 返回，当前函数应该结束执行。
+            return resolve(resolvedData)
+          }
+        }, (error) => {
+          return reject(error)
+        })
+      })(i)
+    }
+  })
+}
+
+myPromiseAll([1, 2, 3]).then((value) => {
+  console.log(value)
+})
+
+// Promise.all() Promise.resolve() Promise.race() Promise.reject() 是 promise 的静态方法，如下方式添加关系
+function ParentFunc () {
+}
+ParentFunc.child = () => {
+  console.log('child')
+}
+ParentFunc.child()
